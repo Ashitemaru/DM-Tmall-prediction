4529ba47195a12901b21f9efc86200ce53f2b641
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2022-06-26 15:23:57 +0800

    Add report

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 config = {
-    "mode": "analysis",
+    "mode": "train",
 
     "user_info_path": "../data/data_format1/user_info_format1.csv",
     "user_log_path": "../data/data_format1/user_log_format1.csv",
